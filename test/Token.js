d0de5418107d7d6fa56b48fa621b0e7c92ede02d
Andy Lee <dosel.korea@gmail.com>
2023-04-01 21:50:16 +0900

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -7,11 +7,16 @@ const tokens = (n) => {
 
 describe('Token', ()=> {
 	let token 
+	let accounts
+	let deployer
 
 	beforeEach(async () =>{
 		//Fetch Token From Blockchain
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
 	})
 
 	describe('Deployment', () => {
@@ -30,10 +35,14 @@ describe('Token', ()=> {
 
 		it('Has correct decimals', async () => {
 			expect(await token.decimals()).to.equal(decimals)
-		})
+		}) 
 
 		it('Has correct total supply', async () => {
 			expect(await token.totalSupply()).to.equal(totalSupply)
+		})
+
+		it('Assigns total supply to deployer', async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
 	})
 
