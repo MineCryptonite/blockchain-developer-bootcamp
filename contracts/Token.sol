6b75caac7cc1e60a78c00dc800d6a99e08acc132
Andy Lee <dosel.korea@gmail.com>
2023-04-02 11:26:52 +0900

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,8 +11,11 @@ contract Token {
 
 	// Track Balances
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
 	event Transfer(address indexed from, address indexed to, uint256 value);
+
+	event Approval(address indexed owner, address indexed spender, uint256 value);
 
 
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
@@ -22,7 +25,7 @@ contract Token {
 		balanceOf[msg.sender] = totalSupply;
 	} 
 
-	function transfer(address _to, uint256 _value) public returns (bool success){
+	function transfer(address _to, uint256 _value) public returns (bool success) {
 		// Require that sender has enough tokens to spend
 		require(balanceOf[msg.sender] >= _value);
 		require(_to != address(0));
@@ -34,6 +37,13 @@ contract Token {
 		// Emit Event
 		emit Transfer(msg.sender, _to, _value);
 
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		require(_spender != address(0));
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 }
