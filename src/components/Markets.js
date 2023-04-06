f9a158d77bc80f02a19b7b84b1427373e60a0deb
Andy Lee <dosel.korea@gmail.com>
2023-04-07 03:22:41 +0900

    21. Deposits pt 2

--- src/components/Markets.js
+++ src/components/Markets.js
@@ -1,36 +1,35 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 
-import config from '../config.json';
+import config from '../config.json'
 
-import { loadTokens } from '../store/interactions';
+import { loadTokens } from '../store/interactions'
 
 const Markets = () => {
-  const provider = useSelector(state => state.provider.connection)	
+  const provider = useSelector(state => state.provider.connection)
   const chainId = useSelector(state => state.provider.chainId)
 
   const dispatch = useDispatch()
 
   const marketHandler = async (e) => {
-  	loadTokens(provider, (e.target.value).split(','), dispatch)
+    loadTokens(provider, (e.target.value).split(','), dispatch)
   }
 
   return(
     <div className='component exchange__markets'>
       <div className='component__header'>
-      	<h2>Select Market</h2>
+        <h2>Select Market</h2>
       </div>
 
       {chainId && config[chainId] ? (
-		<select name="markets" id="markets" onChange={marketHandler}>
-      		<option value={`${config[chainId].DApp.address},${config[chainId].mETH.address}`}>DApp / mETH</option>
-      		<option value={`${config[chainId].DApp.address},${config[chainId].mDAI.address}`}>DApp / mDAI</option>
-      	</select>
+        <select name="markets" id="markets" onChange={marketHandler}>
+          <option value={`${config[chainId].DApp.address},${config[chainId].mETH.address}`}>DApp / mETH</option>
+          <option value={`${config[chainId].DApp.address},${config[chainId].mDAI.address}`}>DApp / mDAI</option>
+        </select>
       ) : (
-      	<div>
-      		<p>Not Deployed to Network</p>
-      	</div>
+        <div>
+          <p>Not Deployed to Network</p>
+        </div>
       )}
-      
 
       <hr />
     </div>
